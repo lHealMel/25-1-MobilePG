683003ac230acc9963ce08fbdf8bcc184a0505f0
lHealMel <mtn2072@gachon.ac.kr>
2025-04-08 23:02:01 +0900

    Test 1st commit & push

--- API/main.py
+++ API/main.py
@@ -1,11 +1,12 @@
 from google import genai
 import os
 
+# hide API key
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents="Explain how AI works",
+    contents="한국어 테스트!",
 )
 
 print(response.text)
