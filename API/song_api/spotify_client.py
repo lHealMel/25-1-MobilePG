00f7887a8347df2c45c2842eb8cce0d5e4ddf0e1
lHealMel <mtn2072@gmail.com>
2025-06-01 03:58:54 +0900

    API Update from desktop(#5) * Modify Gemini API Model

--- API/song_api/spotify_client.py
+++ API/song_api/spotify_client.py
@@ -6,18 +6,15 @@ import pprint
 def song_search(artist, song_name):
     client_id = '17969602a6a74820b44f5cacaad948ba'
     secret = 'a25f6e10ce724d8189c556401d4ba499'
-    client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret = secret)
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     query = f'artist:{artist} track:{song_name}'
     print(f"Spotify search query: {query}")
-    results = sp.search(q=query, type='track', limit = 1)
+    results = sp.search(q=query, type='track', limit=1)
 
     return results
 
 
 if __name__ == "__main__":
-    result = song_search(artist="뉴진스", song_name="Hype Boy")
-    pprint.pprint(result)
-
     result_iu = song_search(artist="IU", song_name="Love wins all")
     pprint.pprint(result_iu)
